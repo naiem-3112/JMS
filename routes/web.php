f793f6318063034e2cf5ec335a43362ae2474a6e
ShahadatPOM <naiem.3112@gmail.com>
2020-06-04 20:48:48 +0600

    todo initialize

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('logout', function () {
+    auth()->logout();
+    return redirect()->to('/');
+});
+
+Route::get('home', function () {
+    $user = auth()->user();
+    return 'welcome back  ' . $user->name;
+});
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
