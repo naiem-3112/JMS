6ea70aea9284b472ea675d08fb63090048069092
ShahadatPOM <naiem.3112@gmail.com>
2020-09-11 05:22:42 -0700

    front home page initialize

---
+++ resources/views/front/home.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Journal Management System</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <style>
+        .full{
+            width: 1140px;
+        }
+        .container{
+            margin-top: 5px;
+        }
+        
+        .social{
+            float: left;
+        }
+
+        .log-reg{
+            float: right;
+        }
+
+        .hr{
+            
+        }
+    </style>
+
+</head>
+<body>
+    <div class="container full">
+        <div class="row">
+            <div class="col-4">
+                <div class="social">
+                    <i class="fa fa-facebook-square"></i>
+                    <i class="fa fa-twitter-square"></i>
+                    <i class="fa fa-linkedin-square"></i>
+                </div>
+            </div>
+            <div class="col-8">
+                <div class="log-reg">
+                    <button class="btn btn-info btn-sm">Login</button>
+                    <button class="btn btn-info btn-sm">Registration</button>
+                </div>
+            </div>
+        </div>
+        <span class="hr"></span>
+    </div>
+
+
+
+
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,12 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::post('/upload', 'UserController@upload_avatar');
+
+Route:: get('/', 'FrontController@home')->name('journal-front.home');
 
 Auth::routes();
