01896515b1aad88eb8e399241d2f40204c38e4dc
ShahadatPOM <naiem.3112@gmail.com>
2020-09-17 23:35:36 +0600

    nothing

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -26,7 +26,7 @@ class VerificationController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::front;
+    protected $redirectTo = RouteServiceProvider::dashboard;
 
     /**
      * Create a new controller instance.
