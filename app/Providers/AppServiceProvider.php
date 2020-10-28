5ecbadab0670476357dc633c5dcc7034f9a3ea72
ShahadatPOM <naiem.3112@gmail.com>
2020-10-28 10:52:48 +0600

    publisher get mark menuscript not solved

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,18 +28,30 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        
         $new_user_admin = User::where('status', 0)->where('user_type_id', '!=', 3)->get();
         $new_reviewer = User::where('status', 0)->where('user_type_id', 3)->get();
         $new_menuscript = Menuscript::where('status', 0)->get();
-
+        $assigned_menuscripts = Menuscript::where('status', 2)->get();
+        foreach($assigned_menuscripts as $menuscript){
+        
+        $total_reviewer = $menuscript->rev_menus->count();
+        $total_checked = $menuscript->rev_menus->where('status', '!=', 0)->count();
+        
+        if($total_reviewer == $total_checked){
+            $marked_menuscript = $menuscript;
+        }else{
+            $marked_menuscript = 0;
+        }
+        dd($marked_menuscript->count());
+        // ---------problem why give value 2 it should be 1
+        
+    }
         // for using Auth
         view()->composer('*', function ($view) {
         $assign_menuscript_reviewer = ReviewerMenuscript::where('status', 0)->where('reviewer_id', Auth::id())->get();
         $view->with('assign_menuscript_reviewer', $assign_menuscript_reviewer );    
     });  
 
-        View::share(['new_user_admin' => $new_user_admin, 'new_reviewer' => $new_reviewer, 
-        'new_menuscript' => $new_menuscript,]);
+        View::share(['new_user_admin' => $new_user_admin, 'new_reviewer' => $new_reviewer, 'new_menuscript' => $new_menuscript, 'marked_menuscript' => $marked_menuscript]);
     }
 }
