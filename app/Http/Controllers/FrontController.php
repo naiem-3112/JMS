9b88fe7f5f94f0d041568bc9ec9147389c6c40b5
ShahadatPOM <naiem.3112@gmail.com>
2020-11-13 08:34:38 +0600

    front

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -8,8 +8,9 @@ use App\Menuscript;
 class FrontController extends Controller
 {
     public function home(){
+        $today = date('Y-m-d H:i:s');
         $menuscripts = Menuscript::where('status', 1)->get();
-        return view('front.home', compact('menuscripts'));
+        return view('front.dashboard', compact('menuscripts', 'today'));
     }
 
 }
