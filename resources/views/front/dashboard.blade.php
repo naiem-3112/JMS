36fb0cef1703fe03cc543c958e112bb62683c769
ShahadatPOM <naiem.3112@gmail.com>
2020-11-15 03:27:13 +0600

    front design coded

--- resources/views/front/dashboard.blade.php
+++ resources/views/front/dashboard.blade.php
@@ -5,6 +5,8 @@
 	<title>Journal Management System</title>
 	<!--	Google Fonts-->
 	<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+
 <!--	Font Awesome-->
 	<link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('css/front.css') }}">
@@ -75,8 +77,7 @@
 			<ul>
 				<li><a href="#">home</a></li>
 				<li><a href="#">about us</a></li>
-				<li><a href="#">services</a></li>
-				<li><a href="#">portfolio</a></li>
+				<li><a href="#">Menuscripts</a></li>
 				<li><a href="#">contact us</a></li>
 			</ul>
 		</div>
@@ -86,22 +87,7 @@
 <!--banner part-->
 <div class="full-wr full-banner">
 	<div class="main-wr main-banner">
-		<div class="banner-text">
-			<h2>Simple, Beautiful <span>and Amazing</span></h2>
-			<p>
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci beatae ea excepturi molestiae nemo obcaecati perferendis porro quam repudiandae, voluptatem. Adipisci beatae ea excepturi molestiae nemo obcaecati perferendis porro quam repudiandae, voluptatem.
-			</p>
-			<div class="banner-btn">
-				<a href="#">Download</a>
-			</div>
-			<div class="banner-btn">
-				<a href="#">Learn More</a>
-			</div>
-
-		</div>
-	</div>
-	<div class="banner-hand-img">
-		<img src="images/hand-img.png" alt="hand img">
+		
 	</div>
 </div>
 <!--summarise part-->
@@ -189,10 +175,75 @@
 		<div class="clr"></div>
 	</div>
 </div>
-<!--testimonial part-->
-<div class="full-wr full-test">
+
+
+<!--summarise part-->
+<div class="full-wr full-summarise">
 	<div class="main-wr">
-		<div id="hexagon"></div>
+		<div class="summarise-top">
+			<div class="section-title">
+				<h3>Contact Us</h3>
+				<p>All the time we love to communicate</p>
+			</div>
+		</div>
+		<div class="summarise-bottom">
+			
+		</div>
+		<div class="clr"></div>
+	</div>
+<iframe style="margin-top: 20px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7296.213792725755!2d90.38516432509185!3d23.885827848068857!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c46b6de8f207%3A0x638eb6830d10167d!2sSector%2010%2C%20Dhaka%201230!5e0!3m2!1sen!2sbd!4v1605384624154!5m2!1sen!2sbd" width="100%" height="400" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+</div>
+
+<!--footer part-->
+<div class="full-wr full-footer">
+	<div class="main-wr">
+		<div class="footer-bottom">
+			<div class="footer-col">
+				<div class="footer-content">
+					<h4>Menu</h4>
+					<h5>Home</h5>
+					<h5>About Us</h5>
+					<h5>Contact Us</h5>
+				</div>
+			</div>
+			<div class="footer-col">
+				<div class="footer-content">
+					<h4>Connect</h4>
+					<h5>Linkedin</h5>
+					<h5>Facebook</h5>
+					<h5>Twitter</h5>
+				</div>
+			</div>
+			<div class="footer-col">
+				<div class="footer-content">
+					<h4>Address</h4>
+					<p>H:4; R:6/A; Uttara, Dhaka-1230</p>
+				</div>
+			</div>
+			<div class="footer-col">
+				<div class="footer-content">
+					<h4>About</h4>
+					<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta, quaerat.</p>
+				</div>
+			</div>
+			<div class="footer-col">
+				<div class="footer-content">
+					<div class="footer_right">
+						<div class="footer_right_logo">
+							<a href="#"><img src="{{asset('back_temp/dist/img/favicon.png')}}"></a>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<div class="clr"></div>
+	</div>
+</div>
+<div style="background: #BD070E" class="full-wr">
+	<div class="main-wr">
+		<p style="text-align: center; color: #ffffff">Copyright © 2020 Mohammad Tamim Rahman </p>
 	</div>
 </div>
 </body>
