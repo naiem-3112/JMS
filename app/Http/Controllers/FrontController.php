ab0f092db22b7556e8dd85f99f6edde6604a04d2
Tamim-Rahman <ttamim.tr@gmail.com>
2020-11-20 12:33:39 +0600

    done

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -9,7 +9,7 @@ class FrontController extends Controller
 {
     public function home(){
         $today = date('Y-m-d H:i:s');
-        $menuscripts = Menuscript::where('status', 4)->get();
+        $menuscripts = Menuscript::where('status', 3)->get();
         return view('front.dashboard', compact('menuscripts', 'today'));
     }
 
@@ -18,7 +18,7 @@ class FrontController extends Controller
 
         $menuscript = Menuscript::find($id);
         $category_id = $menuscript->category_id;
-        $menuscripts = Menuscript::where('category_id', $category_id)->where('status', 4)->get();
+        $menuscripts = Menuscript::where('category_id', $category_id)->where('status', 3)->get();
 
         return view('front.categoryMenuscript', compact('menuscript', 'today', 'menuscripts'));
     }
